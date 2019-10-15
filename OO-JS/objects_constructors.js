fdd7633571a044fd697e6ee500f9bd30c1c90e4a
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-10-15 17:01:57 -0400

    fixing commenting

--- OO-JS/objects_constructors.js
+++ OO-JS/objects_constructors.js
@@ -1,31 +1,31 @@
-// // https://app.schoology.com/assignment/2271041776/info
-// function Person(name, email, phone) {
-//     this.name = name;
-//     this.email = email;
-//     this.phone = phone;
-// }
+// https://app.schoology.com/assignment/2271041776/info
+function Person(name, email, phone) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+}
 
-// Person.prototype.greet = function(other) {
-//     console.log("Hello " + other.name + ", I am " + this.name + "!");
-// };
+Person.prototype.greet = function(other) {
+    console.log("Hello " + other.name + ", I am " + this.name + "!");
+};
 
-// // Instantiate an instance object of Person with name of 'Sonny', email of 'sonny@hotmail.com', and phone of '483-485-4948', store it in the variable sonny.
-// let sonny = new Person("Sonny", "sonny@hotmail.com", "483-485-4948");
+// Instantiate an instance object of Person with name of 'Sonny', email of 'sonny@hotmail.com', and phone of '483-485-4948', store it in the variable sonny.
+let sonny = new Person("Sonny", "sonny@hotmail.com", "483-485-4948");
 
-// // Instantiate another person with the name of 'Jordan', email of 'jordan@aol.com', and phone of '495-586-3456', store it in the variable 'jordan'.
-// let jordan = new Person("Jordan", "jordan@aol.com", "495-586-3456");
+// Instantiate another person with the name of 'Jordan', email of 'jordan@aol.com', and phone of '495-586-3456', store it in the variable 'jordan'.
+let jordan = new Person("Jordan", "jordan@aol.com", "495-586-3456");
 
-// // Have sonny greet jordan using the greet method.
-// sonny.greet(jordan);
+// Have sonny greet jordan using the greet method.
+sonny.greet(jordan);
 
-// // Have jordan greet sonny using the greet method.
-// jordan.greet(sonny);
+// Have jordan greet sonny using the greet method.
+jordan.greet(sonny);
 
-// // Write a statement to print the contact info (email and phone) of Sonny.
-// console.log("Sonny's Contact Info: ", sonny.email, sonny.phone);
+// Write a statement to print the contact info (email and phone) of Sonny.
+console.log("Sonny's Contact Info: ", sonny.email, sonny.phone);
 
-// // Write another statement to print the contact info of Jordan.
-// console.log("Jordan's Contact Info: ", jordan.email, jordan.phone);
+// Write another statement to print the contact info of Jordan.
+console.log("Jordan's Contact Info: ", jordan.email, jordan.phone);
 
 // Card Constructor
 
